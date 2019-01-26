e2dbade654b46bdd686121a47b802be88c8d16d0
Amar <amarsgithub@outlook.com>
2019-01-26 15:03:25 -0500

    Changes made

--- listingController.js
+++ listingController.js
@@ -3,12 +3,45 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
+    $scope.listing = {
+      code: "",
+      name: "",
+      coordinates: {
+        latitude: 0,
+        longitude: 0
+      },
+      address: ""
+    };
+
+    $scope.listingsList = [];
+    for (i = 0; i < $scope.listings.length; i++)
+    {
+      $scope.listingsList.push($scope.listings[i]);
+    }
+  
+    $scope.detailedListing = {
+      code: "",
+      name: "",
+      coordinates: {
+        latitude: 0,
+        longitude: 0,
+      },
+      address: ""
+    };
+
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listingsList.push($scope.listing);
+    };
+    $scope.deleteListing = function(index) {
+      var numericalIndex = $scope.listingsList.indexOf(index);
+      $scope.listingsList.splice(numericalIndex, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedListing = index;
+    };
   }
 ]);
